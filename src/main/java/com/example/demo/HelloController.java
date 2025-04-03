3b5b6e36cdab27f7df17efdc58479c633e9b5bfc
Lwonbin <lwb9036@gmail.com>
2025-04-03 09:27:25 +0900

    mtest

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "안녕ds World!";
+        return "안녕dsasdf World!";
     }
 }
